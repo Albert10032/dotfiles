703b8a104220bbc1567367dbb4c7eb80cfdac169
Albert Stark <albert3@mailfence.com>
2025-08-06 14:14:26 +1000

    tweaking the multi-moniter reload scripts to not fuck around with the background (restarting i3 fixes it), and starting network management applets that fail to start in the standard activation for some reason

--- dotfiles/.scripts/laptop_revert.sh
+++ dotfiles/.scripts/laptop_revert.sh
@@ -1,1 +1,5 @@
-xrandr --output eDP --mode 1920x1200 --rate 144.0 --output DisplayPort-0 --off
+#!/bin/bash
+xrandr --output eDP --primary --mode 1920x1200 --rate 144.0 --output DisplayPort-0 --off
+i3-msg restart
+i3-msg exec nm-applet
+i3-msg exec blueman-applet
