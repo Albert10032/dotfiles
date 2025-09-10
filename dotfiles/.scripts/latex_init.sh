b53997240e42c796c50ee14a1bdb3f224ce8f2c6
Albert Stark <albert3@mailfence.com>
2025-09-10 13:32:03 +1000

    new template for beamer presentation

--- dotfiles/.scripts/latex_init.sh
+++ dotfiles/.scripts/latex_init.sh
@@ -2,5 +2,22 @@
 
 echo "RUNNING LATEX INIT"
 
-cp $HOME/.doc_templates/main.tex $PWD
-cp $HOME/.doc_templates/bibliography.bib $PWD
+function latex_doc () {
+    cp $HOME/.doc_templates/main.tex $PWD
+    cp $HOME/.doc_templates/bibliography.bib $PWD
+    echo "DOCUMENT"
+}
+
+function latex_presentation () {
+    cp $HOME/.doc_templates/presentation.tex $PWD
+    cp $HOME/.doc_templates/bibliography.bib $PWD
+    echo "PRESENTATION"
+}
+
+if [ $1 = "-doc" ]; then
+    latex_doc
+elif [ $1 = "-pres" ]; then
+    latex_presentation
+else
+    echo "use -doc or -pres with this script!"
+fi
